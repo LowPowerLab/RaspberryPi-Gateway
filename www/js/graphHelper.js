003c2197a228a133683bb4e14643eccbc1bd120a
Felix Rusu <felix@lowpowerlab.com>
2020-04-19 20:50:23 -0400

    graphs 24hr data, no spellcheck on forms

--- www/js/graphHelper.js
+++ www/js/graphHelper.js
@@ -9,12 +9,12 @@ var graphView =
     this.start = ((new Date()).getTime())-(3600000*hours);	//Get start time
     this.end = (new Date()).getTime();	//Get end time
   },
-
+  
   'resetDomain':function() {
-    this.start = +new Date - (3600000*8.0); //show 8 hours worth of data at first load
+    this.start = +new Date - (3600000*24.0); //show 24 hours worth of data at first load
     this.end = +new Date;
   },
-
+  
   'zoomout':function ()
   {
     var time_window = this.end - this.start;
